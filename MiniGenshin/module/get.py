a35c861fbc2924f66bdff37faa21c2eb01dbecfc
NotXander <xandercode2@gmail.com>
2025-04-02 14:03:44 -0700

    copy

--- MiniGenshin/module/get.py
+++ MiniGenshin/module/get.py
@@ -1,4 +1,6 @@
 from contextlib import contextmanager
+import msvcrt
+
 if __name__ == "__main__":
     from colors import Colors
 else:
@@ -6,33 +8,20 @@ else:
 
 class GetKey():
     def __init__(self):
-        self.t = __import__("termios")
-        self.tty = __import__("tty")
-        self.sys = __import__("sys")
-        self.ARROWKEYS = {"\x1b[A": "up", "\x1b[B": "down", "\x1b[C": "right", "\x1b[D": "left", "\n": "enter"}
+        self.ARROWKEYS = {b"\xe0H": "up", b"\xe0P": "down", b"\xe0M": "right", b"\xe0K": "left", b"\r": "enter"}
+    
     @contextmanager
     def context(self):
-        fd = self.sys.stdin.fileno()
-        old_settings = self.t.tcgetattr(fd)
-        self.tty.setcbreak(fd)
-        try:
-            yield
-        finally:
-            self.t.tcsetattr(fd, self.t.TCSADRAIN, old_settings)
+        yield  # No special handling needed for Windows
+
     def getKey(self, withKeys: bool = False):
         with self.context():
-            if withKeys:
-                char = self.sys.stdin.read(1)
-                if char == "\x1b":
-                    char += self.sys.stdin.read(2)
-            else:
-                char = self.sys.stdin.read(1)
+            char = msvcrt.getch()
+            if withKeys and char in {b"\xe0", b"\x00"}:  # Handle special keys
+                char += msvcrt.getch()
         if withKeys:
             return self.ARROWKEYS.get(char)
-            # return ord(char)
-        return char
-
-
+        return char.decode("utf-8")
 
 def SelectionMenu(question: str, options: list[str], wipeTerm: bool, yesNo: bool = False):
     index = 0
@@ -46,7 +35,7 @@ def SelectionMenu(question: str, options: list[str], wipeTerm: bool, yesNo: bool
                 print(f"{Colors.RESET}> {possibleColors[i % 4]}{v}{Colors.RESET}")
             else:
                 print(f"{possibleColors[i % 4]}{v}{Colors.RESET}")
-        # key = GetKey().getKey(True)
+        
         key = GetKey().getKey(True)
         if key == "up":
             index = index - 1 if index - 1 >= 0 else len(options) - 1
@@ -55,6 +44,4 @@ def SelectionMenu(question: str, options: list[str], wipeTerm: bool, yesNo: bool
         if key == "enter" and not yesNo:
             return f"{options[index].lower()}"
         if key == "enter" and yesNo:
-            if index == 0:
-                return True
-            return False
+            return index == 0
